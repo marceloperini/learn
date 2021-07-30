e07683abf2f6e46c1f1e701c241c30bffd7588bc
Marcelo Perini Veloso <marcelo.perini.veloso@pm.me>
2021-07-29 22:23:27 -0300

    react lessons 51 and 52

--- react/indecision-app/src/app.js
+++ react/indecision-app/src/app.js
@@ -1,7 +1,16 @@
-// import './utils'
+// import subtract, { square, add } from './utils';
 
-import { square, add } from './utils';
+// console.log('app.js is running...');
+// console.log(square(4));
+// console.log(add(1, 2));
+// console.log(subtract(100, 81))
 
-console.log('app.js is running...');
-console.log(square(4));
-console.log(add(1, 2));
+import isSenior, { isAdult, canDrink } from "./person";
+
+console.log('Does 18 age is adult? ', isAdult(18));
+console.log('Does 10 age is adult? ', isAdult(10));
+
+console.log('Does 18 age can drink? ', canDrink(18))
+console.log('Does 10 age can drink? ', canDrink(10))
+
+console.log('Does 65 a senior age? ', isSenior(65));
---
+++ react/indecision-app/src/person.js
@@ -0,0 +1,5 @@
+const isAdult = (age) => age >= 18
+const canDrink = (age) => age >= 18
+const isSenior = (age) => age >= 60
+
+export { isAdult, canDrink, isSenior as default }
