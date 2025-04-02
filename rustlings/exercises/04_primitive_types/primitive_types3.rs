cdd11bee65ba4e9481729b0da33469f2fb1cd6e5
Marcelo Perini Veloso <marcelo.perini.veloso@pm.me>
2025-04-02 20:27:25 +0100

    finish primitive_types exercises

--- rustlings/exercises/04_primitive_types/primitive_types3.rs
+++ rustlings/exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,10 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let mut a: Vec<i32> = Vec::new();
+
+    for i in 0..100 {
+        a.push(i);
+    }
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
