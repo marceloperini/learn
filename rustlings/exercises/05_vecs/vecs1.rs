25946c2a655f7bb4721424899d41df023e4101ac
Marcelo Perini Veloso <marcelo.perini.veloso@pm.me>
2025-04-19 11:00:14 +0100

    exercise 5

--- rustlings/exercises/05_vecs/vecs1.rs
+++ rustlings/exercises/05_vecs/vecs1.rs
@@ -1,9 +1,14 @@
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
-    let a = [10, 20, 30, 40]; // Array
+    let mut a = [10, 20, 30, 40]; // Array
+    let mut v: Vec<i32> = Vec::new();
 
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
     // let v = ???;
+
+    for elem in a.iter_mut() {
+        v.push(*elem);
+    }
 
     (a, v)
 }
